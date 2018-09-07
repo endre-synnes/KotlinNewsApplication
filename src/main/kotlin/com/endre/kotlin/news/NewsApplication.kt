391763887790658513656e652390494e2bd3fc3f
synend16 <synend16@student.westerdals.no>
2018-09-07 11:48:25 +0200

    enabled swagger

--- src/main/kotlin/com/endre/kotlin/news/NewsApplication.kt
+++ src/main/kotlin/com/endre/kotlin/news/NewsApplication.kt
@@ -2,8 +2,10 @@ package com.endre.kotlin.news
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
+@EnableSwagger2
 class NewsApplication
 
 fun main(args: Array<String>) {
